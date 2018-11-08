4812f816bfcf38a1be240c0df2d7b7ab336704ea
lisali42 <lisali42@hotmail.ca>
2018-11-08 14:44:15 -0800

    Can't get newline

--- SV parse.py
+++ SV parse.py
@@ -8,14 +8,22 @@ steptwo=stepone.split(';')
 
 file.close()
 
-stepthree = re.search(r'\(([^)]+)', steptwo[0])
+stepthree = re.search(r'\(([^)]+)', steptwo[0])             #Regex search, inside brackets, ^) excludes ')'
 
-stepfour = stepthree.group(0).split('(')
+stepfour = stepthree.group(0).split('(')                    #Get rid of the '('
 
-stepfive = stepfour.split(',')
+stepfive = re.compile('input (.*)', flags=re.DOTALL)
+
+stepsix = re.search(stepfive,stepfour[1])
+
+#stepfive = stepfour[1].split(',')
 
 file = open('output.txt', mode='w')
 
-file.write(stepfour[1])
+if stepfive:
+    file.write(stepsix.group(0))
+
+#for i in stepfive:
+#    file.write(stepfive[1])
 
 file.close()
