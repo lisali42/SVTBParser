9ad1f6936453d262b23de7975374ec5eefc96fd4
lisali42 <lisali42@hotmail.ca>
2019-02-18 19:59:48 -0800

    Update SV parse.py

--- SV parse.py
+++ SV parse.py
@@ -4,21 +4,33 @@ file = open ('chipmunks.sv', mode = 'r')
 
 stepone=file.read()
 
-moduleheader=stepone.split(';')
+moduleheader = stepone.split(';')
+
+# Getting the module name
+spaceSplit = stepone.split(' ')
+bracketSplitModName = spaceSplit[1].split('(')
+modName = bracketSplitModName[0]
 
 file.close()
 
 bracketsRegexObj = re.search(r'\(([^)]+)', moduleheader[0])             #Regex search, inside brackets, ^) excludes ')'
 
 IOlist = bracketsRegexObj.group(0).split('(')                    #Get rid of the '('
-#input parse
+
+# input parse
+
 inputRegexObj = re.compile(r'(?<=input )\w+')                    #Finds all input values
 inputRegexObjBus = re.compile(r'(?<=input )\[\w+\D\w+\] \w+')
 
-inputList = re.findall(inputRegexObj,IOlist[1])                  #inputList is a array that contains all inputs
-inputListbus = re.findall(inputRegexObjBus,IOlist[1])
+inputList = re.findall(inputRegexObj, IOlist[1])                  #inputList is a array that contains all inputs
+inputListbus = re.findall(inputRegexObjBus, IOlist[1])
 
 file = open('output.txt', mode='w')
+
+file.write('module tb_')
+file.write(modName)
+file.write('();')
+file.write('\n')
 
 if inputList:
     for x in range(len(inputList)):
